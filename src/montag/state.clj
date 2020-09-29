801ac5fee086cce286ec9900b4fcdde85869e123
Wanderson Ferreira <iagwanderson@gmail.com>
2020-09-29 17:44:17 -0300

    start with a general best guess of loaded vars

--- src/montag/state.clj
+++ src/montag/state.clj
@@ -1,12 +1,16 @@
-(ns montag.state)
+(ns montag.state
+  (:require [montag.find :as -find]))
+
+(defonce ^:private all-vars (-find/find-all-vars))
 
 (def db (atom {:fn-inside []
                :fn-references []
                :fn-clicked nil
-               :all-fns {}
+               :all-fns all-vars
                :filter-fns []}))
 
 (defn update-vars! [var]
   (let [name (str (:ns (meta var)) "/" (:name (meta var)))]
     (swap! db update :filter-fns conj name)
     (swap! db assoc-in [:all-fns name] var)))
+
