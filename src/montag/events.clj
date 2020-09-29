4c4cc3719a3ef8473e9e81744f2add9a08a58c05
Wanderson Ferreira <iagwanderson@gmail.com>
2020-09-29 17:45:04 -0300

    handler of events

---
+++ src/montag/events.clj
@@ -0,0 +1,24 @@
+(ns montag.events
+  (:require [montag.filter :as -filter]
+            [montag.find :as -find]
+            [montag.state :as -state]))
+
+(defmulti handler (fn [event] (:event/type event)))
+
+(defmethod handler :filter/remove-all-ns
+  [_]
+  (swap! -state/db assoc :filter-fns (sort (keys (:all-fns @-state/db)))))
+
+(defmethod handler :filter/project-ns
+  [_]
+  (let [vars (-find/find-all-vars-current-project)
+        alls (:all-fns @-state/db)]
+    (swap! -state/db assoc :filter-fns (keys vars))
+    (swap! -state/db assoc :all-fns (merge vars alls))))
+
+(defmethod handler :filter/current-ns
+  [_]
+  (when-let [fn-name (:fn-clicked @-state/db)]
+    (->> (:all-fns @-state/db)
+         (-filter/filter-namespace fn-name )
+         (swap! -state/db assoc :filter-fns))))
